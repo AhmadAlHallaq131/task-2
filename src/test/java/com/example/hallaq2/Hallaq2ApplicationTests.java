00ac2312eb2dd281eb91d8ce858be24422eeb5c9
AhmadAlHallaq131 <ahmadalhallaq131@gmail.com>
2023-11-12 15:22:01 +0300

    task

---
+++ src/test/java/com/example/hallaq2/Hallaq2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hallaq2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hallaq2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
